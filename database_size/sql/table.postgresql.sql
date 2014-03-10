40a610c814119aa9750b7bba6ceb07ed9afa11c5
chris <chris@coronis>
2014-03-09 20:41:39 -0400

    Added aggregates to view.

--- database_size/sql/table.postgresql.sql
+++ database_size/sql/table.postgresql.sql
@@ -10,4 +10,19 @@ SELECT  CONCAT(CAST(schemaname AS VARCHAR), '-', CAST(tablename AS VARCHAR)) AS 
         tablename AS table_name,
         tableowner AS table_owner,
         pg_total_relation_size(schemaname || '.' || tablename) AS size_in_bytes
+FROM    pg_tables
+UNION ALL
+SELECT  CONCAT(CAST(schemaname AS VARCHAR), '-byschema') AS id,
+        schemaname AS schema_name,
+        'ALL' AS table_name,
+        'ALL' AS table_owner,
+        SUM(pg_total_relation_size(schemaname || '.' || tablename))::bigint AS size_in_bytes
+FROM    pg_tables
+GROUP BY schemaname
+UNION ALL
+SELECT  'all-all' AS id,
+        'ALL' AS schema_name,
+        'ALL' AS table_name,
+        'ALL' AS table_owner,
+        SUM(pg_total_relation_size(schemaname || '.' || tablename))::bigint AS size_in_bytes
 FROM    pg_tables;
