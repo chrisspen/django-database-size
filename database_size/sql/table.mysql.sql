e767fc631e19d8aa077358be60cfa216cf3bfbe5
chris <chris@coronis>
2014-03-13 16:01:33 -0400

    Updated mysql view.

--- database_size/sql/table.mysql.sql
+++ database_size/sql/table.mysql.sql
@@ -10,4 +10,19 @@ SELECT  CONCAT(table_schema, '-', table_name) AS id,
         table_name AS table_name,
         null AS table_owner, -- TODO: not available in MySQL?!
         (data_length + index_length) AS size_in_bytes
+FROM    information_schema.TABLES
+UNION ALL
+SELECT  CONCAT(table_schema, '-byschema') AS id,
+        table_schema AS schema_name,
+        'ALL' AS table_name,
+        'ALL' AS table_owner, -- TODO: not available in MySQL?!
+        SUM(data_length + index_length) AS size_in_bytes
+FROM    information_schema.TABLES
+GROUP BY table_schema
+UNION ALL
+SELECT  'all-all' AS id,
+        'ALL' AS schema_name,
+        'ALL' AS table_name,
+        'ALL' AS table_owner, -- TODO: not available in MySQL?!
+        SUM(data_length + index_length) AS size_in_bytes
 FROM    information_schema.TABLES;
