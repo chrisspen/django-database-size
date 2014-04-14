5da6987ea025d128e212d8be64128586328cca27
chris <chris@coronis>
2014-04-14 13:01:18 -0400

    Added multiple-database support.

--- database_size/admin.py
+++ database_size/admin.py
@@ -1,7 +1,48 @@
 from django.contrib import admin
+from django.db import connections
+from django.contrib.admin import SimpleListFilter
 
 import models
 import utils
+
+class SelectDatabaseListFilter(SimpleListFilter):
+    
+    title = 'database'
+    
+    parameter_name = 'database'
+    
+    default_value = 'default'
+    
+    def __init__(self, request, params, model, model_admin):
+        self.parameter_val = None
+        try:
+            self.parameter_val = request.GET.get(self.parameter_name, self.default_value)
+        except Exception, e:
+            pass
+        super(SelectDatabaseListFilter, self).__init__(request, params, model, model_admin)
+
+    def lookups(self, request, model_admin):
+        """
+        Must be overriden to return a list of tuples (value, verbose value)
+        """
+        return [(conn, conn) for conn in connections]
+
+    def choices(self, cl):
+        for lookup, title in self.lookups(None, None):
+            yield {
+                'selected': self.parameter_val == lookup,
+                'query_string': cl.get_query_string({
+                    self.parameter_name: lookup,
+                }, []),
+                'display': title,
+            }
+
+    def queryset(self, request, queryset):
+        """
+        Returns the filtered queryset.
+        """
+        queryset._db = self.parameter_val
+        return queryset
 
 class TableAdmin(admin.ModelAdmin):
     
@@ -17,6 +58,7 @@ class TableAdmin(admin.ModelAdmin):
     list_filter = (
         'schema_name',
         'table_owner',
+        SelectDatabaseListFilter,
     )
     search_fields = (
         'schema_name',
